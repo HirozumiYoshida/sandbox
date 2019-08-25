09cd27bfe88557771ee011dcf2229e4fe7c1c72b
HirozumiYoshida <ips.hyoshida@gmail.com>
2019-08-25 20:58:13 +0900

    Initial Commit

---
+++ sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sandbox
+//
+//  Created by 吉田　裕澄 on 2019/08/25.
+//  Copyright © 2019 吉田　裕澄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

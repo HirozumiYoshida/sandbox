935a59c22d416a0dad54ace9a791d02570c75986
HirozumiYoshida <ips.hyoshida@gmail.com>
2019-10-07 15:26:34 +0900

    2019.10.07 実装開始

---
+++ sandbox/LBPB100ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LBPB100ViewController.swift
+//  sandbox
+//
+//  Created by 吉田　裕澄 on 2019/10/04.
+//  Copyright © 2019 吉田　裕澄. All rights reserved.
+//
+
+import UIKit
+
+class LBPB100ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- sandbox/ViewController.swift
+++ sandbox/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    let TODO = ["電源入れる。", "ログインする。", "xCode起動する。"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = TODO[indexPath.row]
+        return cell
+    }
+    
 
 }
 
